024142e980ada3e5e7e5380d1ff7e79e015c623f
Jordandt22 <jordandt123411@gmail.com>
2025-09-08 17:46:43 -0700

    (Client): Finished Next.js Setup.

---
+++ client/src/app/layout.js
@@ -0,0 +1,24 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+export const metadata = {
+  title: "RadiatorRepairHub",
+  description: "RadiatorRepairHub helps you find trusted auto radiator repair shops near you. Browse by city, compare services, and keep your car running cool.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
