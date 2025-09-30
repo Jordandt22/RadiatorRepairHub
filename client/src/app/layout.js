482689584e2632a374f4ce38d3c7a6316a369aa6
Jordandt22 <jordandt123411@gmail.com>
2025-09-30 04:06:03 -0700

    (Client): Added Google Analytics

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter, Oswald } from "next/font/google";
 import { Toaster } from "sonner";
+import Script from "next/script";
 import "./globals.css";
 
 // Components
@@ -189,6 +190,24 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.variable} ${oswald.variable} font-sans antialiased`}
       >
+        {/* Google Analytics */}
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+              `}
+            </Script>
+          </>
+        )}
+
         <Navbar />
 
         {children}
