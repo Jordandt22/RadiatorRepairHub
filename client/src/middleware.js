bc47d68e0f3388e592d9608dd7be1371d6e5f078
Jordandt22 <jordandt123411@gmail.com>
2025-10-21 01:55:54 -0700

    Added Maintenance Page.

---
+++ client/src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const maintenanceMode = process.env.MAINTENANCE_MODE === "true";
+  const isMaintenancePage = req.nextUrl.pathname.startsWith("/maintenance");
+  if (maintenanceMode && !isMaintenancePage) {
+    const maintenanceUrl = req.nextUrl.clone();
+    maintenanceUrl.pathname = "/maintenance";
+    return NextResponse.redirect(maintenanceUrl);
+  }
+
+  return NextResponse.next();
+}
+
+// Define which routes the middleware runs on
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
